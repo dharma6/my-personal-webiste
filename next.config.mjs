276892054356c4605491834de01c35c061cfadb6
Dharma Teja Bandaru <dharmatejabandaru@gmail.com>
2025-01-28 21:13:08 -0600

    added security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,64 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value:
+      "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' https: data:;",
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    key: 'X-Powered-By',
+    value: '', // This removes the X-Powered-By header
+  },
+];
+
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'dharmabandaru.com',
+          },
+        ],
+        permanent: true,
+        destination: 'https://dharmabandaru.com/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
