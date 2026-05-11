93e86d69e9e02d59c6f746102d6c3024e1f5da1f
Dharma Teja Bandaru <dharmatejabandaru@gmail.com>
2026-05-10 22:23:45 -0500

    removed og image

--- app/investment-thesis/[slug]/page.js
+++ app/investment-thesis/[slug]/page.js
@@ -7,6 +7,30 @@ export function generateStaticParams() {
   }));
 }
 
+export async function generateMetadata({ params }) {
+  const { slug } = await params;
+  const post = blogPosts.find((p) => p.slug === slug);
+
+  if (!post) return {};
+
+  return {
+    title: post.title,
+    description: post.excerpt,
+    openGraph: {
+      title: post.title,
+      description: post.excerpt,
+      type: 'article',
+      url: `https://dharmabandaru.com/investment-thesis/${slug}`,
+      images: [],
+    },
+    twitter: {
+      card: 'summary',
+      title: post.title,
+      description: post.excerpt,
+    },
+  };
+}
+
 export default async function ArticlePage({ params }) {
   const { slug } = await params;
   return <ArticleContent slug={slug} />;
